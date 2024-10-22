2cc14c8bccdd6809a9ad4437aa2230d79ae7964d
Adeel Malik <amalik@datical.com>
2024-10-22 16:01:33 -0500

    sqlcode

--- alive.sh
+++ alive.sh
@@ -2,7 +2,7 @@ while [ 1 ]
 do
     echo "************************* sqlrepository is alive *************************"
     echo "Current working directory is : `pwd`"
-    ls -alh alive.sh /sqlrepository
+    ls -alh alive.sh /sqlcode
     # ls -alh ./sqlcode
-    sleep 5
+    sleep 15
 done
